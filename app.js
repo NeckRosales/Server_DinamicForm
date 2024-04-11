94a0c4cb12cf6463f53945e763019e96879bc97b
neckr <nvivas@ucol.mx>
2024-04-11 12:13:07 -0600

    server deploy

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ import cors from 'cors'
 import morgan from 'morgan'
 
 const app = express()
+const port = process.env.PORT ?? 3000
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan('dev'))
 app.get('/', (req, res) => {
-  res.send('Puto')
+  res.send('Connected')
 })
 
 app.use('/form', formRoutes)
 dbConnection()
-app.listen(3000, () => {
-  console.log('server on port 3000')
+app.listen(port, () => {
+  console.log(`server on port ${port}`)
 })
--- src/db.js
+++ src/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 export const dbConnection = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/dynamicForm')
+    await mongoose.connect(
+      'mongodb+srv://acernas1:aW4BSFluT0FxnviX@cluster0.yuxjmzz.mongodb.net/?retryWrites=true&w=majority'
+    )
     console.log('Conexion a la base de datos con exito')
   } catch (error) {
     console.error(error)

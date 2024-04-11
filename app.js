491f0e2a555914fcba0536507a77327ca4380f83
neckr <nvivas@ucol.mx>
2024-04-11 12:23:55 -0600

    routes

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ import express from 'express'
 import { dbConnection } from './src/db.js'
 import formRoutes from './src/routes/routes.js'
 import cors from 'cors'
-import morgan from 'morgan'
 
 const app = express()
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
-app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
   res.send('Connected')
 })
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ import { FormController } from '../controllers/forms.js'
 const router = Router()
 
 router.post('/create-form', FormController.createForm)
-router.get('/read-form', FormController.readForm)
+router.get('/get-forms', FormController.readForm)
 // router.put('/update-form', FormController.updateForm)
 router.delete('/delete-form/:id', FormController.deleteForm)
 
